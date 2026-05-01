8b9fbc0a687cd4ba373519fc4dc0108edaef6d42
Garrett Krohn <garrettkrohn@gmail.com>
2026-05-01 12:29:18 -0500

    fix deployment, os specific git

---
+++ git/git_windows.go
@@ -0,0 +1,11 @@
+//go:build windows
+
+package git
+
+import "syscall"
+
+func setSysProcAttr() *syscall.SysProcAttr {
+	return &syscall.SysProcAttr{
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+	}
+}
